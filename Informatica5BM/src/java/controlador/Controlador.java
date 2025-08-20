76bc36335e1acfa2b7a0f9134c10f4a8684db9bc
xportillo-2024040 <xportillo-2024040@kinal.edu.gt>
2025-08-20 11:48:24 -0600

    Fix: Realizando cambios en el controlador del proyecto

--- Informatica5BM/src/java/controlador/Controlador.java
+++ Informatica5BM/src/java/controlador/Controlador.java
@@ -12,6 +12,10 @@ public class Controlador extends HttpServlet {
 
     PropietarioDAO propietarioDAO = new PropietarioDAO();
     PropiedadDAO propiedadDAO = new PropiedadDAO();
+    Propietario propietario = new Propietario();
+    Propiedad propiedad = new Propiedad();
+    int codPropietario;
+    int codPropiedad;
 
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
@@ -23,122 +27,117 @@ public class Controlador extends HttpServlet {
             return;
         }
 
-        switch (menu) {
-            case "Propietario":
-                switch (accion) {
-                    case "Listar":
-                        List<Propietario> listaPropietario = propietarioDAO.listar();
-                        request.setAttribute("propietarios", listaPropietario);
-                        break;
-                    case "Agregar":
-                        String nombrePropietario = request.getParameter("txtNombre");
-                        String telefonoPropietario = request.getParameter("txtTelefono");
-                        Propietario nuevoPropietario = new Propietario();
-                        nuevoPropietario.setNombre(nombrePropietario);
-                        nuevoPropietario.setTelefono(telefonoPropietario);
-                        propietarioDAO.agregar(nuevoPropietario);
-                        break;
-                    case "Editar":
-                        try {
-                            int codPropietario = Integer.parseInt(request.getParameter("codigoPropietario"));
-                            Propietario p = propietarioDAO.listarCodigoPropietario(codPropietario);
-                            request.setAttribute("propietario", p);
-                        } catch (NumberFormatException e) {
-                            System.err.println("Error al parsear el código de propietario para editar: " + e.getMessage());
-                            // Puedes agregar un mensaje de error para el usuario si lo deseas
-                        }
-                        break;
-                    case "Actualizar":
-                        try {
-                            int id = Integer.parseInt(request.getParameter("txtCodigoPropietario"));
-                            String nomPropietario = request.getParameter("txtNombre");
-                            String telPropietario = request.getParameter("txtTelefono");
-                            Propietario propActualizado = new Propietario();
-                            propActualizado.setCodigoPropietario(id);
-                            propActualizado.setNombre(nomPropietario);
-                            propActualizado.setTelefono(telPropietario);
-                            propietarioDAO.actualizar(propActualizado);
-                        } catch (NumberFormatException e) {
-                            System.err.println("Error al parsear el código de propietario para actualizar: " + e.getMessage());
-                        }
-                        break;
-                    case "Eliminar":
-                        try {
-                            int codPropietario = Integer.parseInt(request.getParameter("codigoPropietario"));
-                            propietarioDAO.eliminar(codPropietario);
-                        } catch (NumberFormatException e) {
-                            System.err.println("Error al parsear el código de propietario para eliminar: " + e.getMessage());
-                        }
-                        break;
-                }
-                List<Propietario> listaPropietarios = propietarioDAO.listar();
-                request.setAttribute("propietarios", listaPropietarios);
-                request.getRequestDispatcher("Propietario.jsp").forward(request, response);
-                break;
+        if (menu.equals("Propietario")) {
+            switch (accion) {
+                case "Listar":
+                    List<Propietario> listaPropietario = propietarioDAO.listar();
+                    request.setAttribute("propietarios", listaPropietario);
+                    break;
+                case "Agregar":
+                    String nombrePropietario = request.getParameter("txtNombre");
+                    String telefonoPropietario = request.getParameter("txtTelefono");
+                    propietario.setNombre(nombrePropietario);
+                    propietario.setTelefono(telefonoPropietario);
+                    propietarioDAO.agregar(propietario);
+                    request.getRequestDispatcher("Controlador?menu=Propietario&accion=Listar").forward(request, response);
+                    return;
+                case "Editar":
+                    try {
+                        codPropietario = Integer.parseInt(request.getParameter("codigoPropietario"));
+                        Propietario p = propietarioDAO.listarCodigoPropietario(codPropietario);
+                        request.setAttribute("propietario", p);
+                    } catch (NumberFormatException e) {
+                        System.err.println("Error al parsear el código de propietario para editar: " + e.getMessage());
+                    }
+                    break;
+                case "Actualizar":
+                    try {
+                        int id = Integer.parseInt(request.getParameter("txtCodigoPropietario"));
+                        String nomPropietario = request.getParameter("txtNombre");
+                        String telPropietario = request.getParameter("txtTelefono");
+                        propietario.setCodigoPropietario(id);
+                        propietario.setNombre(nomPropietario);
+                        propietario.setTelefono(telPropietario);
+                        propietarioDAO.actualizar(propietario);
+                    } catch (NumberFormatException e) {
+                        System.err.println("Error al parsear el código de propietario para actualizar: " + e.getMessage());
+                    }
+                    request.getRequestDispatcher("Controlador?menu=Propietario&accion=Listar").forward(request, response);
+                    return;
+                case "Eliminar":
+                    try {
+                        codPropietario = Integer.parseInt(request.getParameter("codigoPropietario"));
+                        propietarioDAO.eliminar(codPropietario);
+                    } catch (NumberFormatException e) {
+                        System.err.println("Error al parsear el código de propietario para eliminar: " + e.getMessage());
+                    }
+                    request.getRequestDispatcher("Controlador?menu=Propietario&accion=Listar").forward(request, response);
+                    return;
+            }
+            List<Propietario> listaPropietarios = propietarioDAO.listar();
+            request.setAttribute("propietarios", listaPropietarios);
+            request.getRequestDispatcher("Propietario.jsp").forward(request, response);
+        } else if (menu.equals("Propiedad")) {
+            List<Propietario> listaPropietariosCombo = propietarioDAO.listar();
+            request.setAttribute("propietarios", listaPropietariosCombo);
 
-            case "Propiedad":
-                // Obtener la lista de propietarios para el combo box
-                List<Propietario> listaPropietariosCombo = propietarioDAO.listar();
-                request.setAttribute("propietarios", listaPropietariosCombo);
-
-                switch (accion) {
-                    case "Listar":
-                        List<Propiedad> listaPropiedad = propiedadDAO.listar();
-                        request.setAttribute("propiedades", listaPropiedad);
-                        break;
-                    case "Agregar":
-                        try {
-                            Propiedad nuevaPropiedad = new Propiedad();
-                            nuevaPropiedad.setTipo(request.getParameter("txtTipo"));
-                            nuevaPropiedad.setDireccion(request.getParameter("txtDireccion"));
-                            nuevaPropiedad.setNumeroHabitaciones(Integer.parseInt(request.getParameter("txtNumeroHabitaciones")));
-                            nuevaPropiedad.setPrecio(request.getParameter("txtPrecio"));
-                            nuevaPropiedad.setCodigoPropietario(Integer.parseInt(request.getParameter("txtCodigoPropietario")));
-                            propiedadDAO.agregar(nuevaPropiedad);
-                        } catch (NumberFormatException e) {
-                            System.err.println("Error al parsear un campo numérico al agregar propiedad: " + e.getMessage());
-                        }
-                        break;
-                    case "Editar":
-                        try {
-                            int codPropiedad = Integer.parseInt(request.getParameter("codigoPropiedad"));
-                            Propiedad pro = propiedadDAO.listarCodigoPropiedad(codPropiedad);
-                            request.setAttribute("propiedad", pro);
-                        } catch (NumberFormatException e) {
-                            System.err.println("Error al parsear el código de propiedad para editar: " + e.getMessage());
-                        }
-                        break;
-                    case "Actualizar":
-                        try {
-                            Propiedad propActualizada = new Propiedad();
-                            propActualizada.setCodigoPropiedad(Integer.parseInt(request.getParameter("txtCodigoPropiedad")));
-                            propActualizada.setTipo(request.getParameter("txtTipo"));
-                            propActualizada.setDireccion(request.getParameter("txtDireccion"));
-                            propActualizada.setNumeroHabitaciones(Integer.parseInt(request.getParameter("txtNumeroHabitaciones")));
-                            propActualizada.setPrecio(request.getParameter("txtPrecio"));
-                            propActualizada.setCodigoPropietario(Integer.parseInt(request.getParameter("txtCodigoPropietario")));
-                            propiedadDAO.actualizar(propActualizada);
-                        } catch (NumberFormatException e) {
-                            System.err.println("Error al parsear un campo numérico al actualizar propiedad: " + e.getMessage());
-                        }
-                        break;
-                    case "Eliminar":
-                        try {
-                            int codPropiedad = Integer.parseInt(request.getParameter("codigoPropiedad"));
-                            propiedadDAO.eliminar(codPropiedad);
-                        } catch (NumberFormatException e) {
-                            System.err.println("Error al parsear el código de propiedad para eliminar: " + e.getMessage());
-                        }
-                        break;
-                }
-                List<Propiedad> listaPropiedades = propiedadDAO.listar();
-                request.setAttribute("propiedades", listaPropiedades);
-                request.getRequestDispatcher("Propiedad.jsp").forward(request, response);
-                break;
+            switch (accion) {
+                case "Listar":
+                    List<Propiedad> listaPropiedad = propiedadDAO.listar();
+                    request.setAttribute("propiedades", listaPropiedad);
+                    break;
+                case "Agregar":
+                    try {
+                        propiedad.setTipo(request.getParameter("txtTipo"));
+                        propiedad.setDireccion(request.getParameter("txtDireccion"));
+                        propiedad.setNumeroHabitaciones(Integer.parseInt(request.getParameter("txtNumeroHabitaciones")));
+                        propiedad.setPrecio(request.getParameter("txtPrecio"));
+                        propiedad.setCodigoPropietario(Integer.parseInt(request.getParameter("txtCodigoPropietario")));
+                        propiedadDAO.agregar(propiedad);
+                    } catch (NumberFormatException e) {
+                        System.err.println("Error al parsear un campo numérico al agregar propiedad: " + e.getMessage());
+                    }
+                    request.getRequestDispatcher("Controlador?menu=Propiedad&accion=Listar").forward(request, response);
+                    return;
+                case "Editar":
+                    try {
+                        codPropiedad = Integer.parseInt(request.getParameter("codigoPropiedad"));
+                        Propiedad pro = propiedadDAO.listarCodigoPropiedad(codPropiedad);
+                        request.setAttribute("propiedad", pro);
+                    } catch (NumberFormatException e) {
+                        System.err.println("Error al parsear el código de propiedad para editar: " + e.getMessage());
+                    }
+                    break;
+                case "Actualizar":
+                    try {
+                        propiedad.setCodigoPropiedad(Integer.parseInt(request.getParameter("txtCodigoPropiedad")));
+                        propiedad.setTipo(request.getParameter("txtTipo"));
+                        propiedad.setDireccion(request.getParameter("txtDireccion"));
+                        propiedad.setNumeroHabitaciones(Integer.parseInt(request.getParameter("txtNumeroHabitaciones")));
+                        propiedad.setPrecio(request.getParameter("txtPrecio"));
+                        propiedad.setCodigoPropietario(Integer.parseInt(request.getParameter("txtCodigoPropietario")));
+                        propiedadDAO.actualizar(propiedad);
+                    } catch (NumberFormatException e) {
+                        System.err.println("Error al parsear un campo numérico al actualizar propiedad: " + e.getMessage());
+                    }
+                    request.getRequestDispatcher("Controlador?menu=Propiedad&accion=Listar").forward(request, response);
+                    return;
+                case "Eliminar":
+                    try {
+                        codPropiedad = Integer.parseInt(request.getParameter("codigoPropiedad"));
+                        propiedadDAO.eliminar(codPropiedad);
+                    } catch (NumberFormatException e) {
+                        System.err.println("Error al parsear el código de propiedad para eliminar: " + e.getMessage());
+                    }
+                    request.getRequestDispatcher("Controlador?menu=Propiedad&accion=Listar").forward(request, response);
+                    return;
+            }
+            List<Propiedad> listaPropiedades = propiedadDAO.listar();
+            request.setAttribute("propiedades", listaPropiedades);
+            request.getRequestDispatcher("Propiedad.jsp").forward(request, response);
         }
     }
-// <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
      * Handles the HTTP <code>GET</code> method.
      *
@@ -176,5 +175,4 @@ public class Controlador extends HttpServlet {
     public String getServletInfo() {
         return "Short description";
     }// </editor-fold>
-
 }
